3db82fc3509afc0e81a52b3aaf6f4aab142cf374
Norma I. Ayala-Rosa <norma.i.ayalarosa@outlook.com>
2018-04-20 07:34:35 -0400

    Add files via upload

---
+++ Exercises/Ex2MailingLabels.sql
@@ -0,0 +1,14 @@
+.echo on
+.headers on 
+
+--Name: Mailing Labels.sql
+--Autho: Nia Rosa
+--Date: February 27, 2018
+
+Select title, firsname, lastname
+Northwind Inc
+address
+city region postalcode country
+[blank line]
+[blank line]
+from hr.Employees
